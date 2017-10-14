a9108f6b390367906108d3a2523a3576c8c1bd59
Jordan Pizza <windowsinstalls@gmail.com>
2017-10-14 10:37:08 -0400

    Added Python Dependancies

--- python-scripts/res.py
+++ python-scripts/res.py
@@ -1,26 +1,62 @@
-#!/usr/bin/python3
-
 import requests
 import json
-import pprint
-import sys
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
-def main():
-    r = requests.post('https://www.blackrock.com/tools/hackathon/portfolio-analysis',
-        data = {
-            'betaPortfolios' :  "SNP500",
-            'calculateExposures' :  "true",
-            'calculatePerformance' :  "true",
-            'positions' :  "AAPL~25|ITOT~25|AGG~25|EFAV~25|",
-            'riskFreeRatePortfolio' :  "LTBILL1-3M"
-        })
+# Risk (0 is Not Risky, 1 is risky) - oneYearRisk
+# upMonths, highDate, highReturn, 
 
-    status = r.status_code
+r = requests.post('https://www.blackrock.com/tools/hackathon/portfolio-analysis',
+  data = {
+	'betaPortfolios' :  "SNP500",
+	"calculateExpectedReturns" :  "true",
+	"calculateExposures" :  "true",
+	"calculatePerformance" :  "true",
+	"calculateRisk" :  "true",
+	"calculateStressTests" :  "true",
+	"positions" :  "ISAAT~0.30|ORRYX~0.70",
+	"riskFreeRatePortfolio" :  "LTBILL1-3M",
+	"scenarios" :  "HIST_20081102_20080911,HIST_20110919_20110720,HIST_20130623_20130520,HIST_20140817_20140101,US10Y_1SD::APB,INF2Y_1SD::APB,USIG_1SD::APB,SPX_1SD::APB,DXY_1SD::APB"
+})
 
-    if status == 200:
-        res = r.json()
-        print(res['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['analyticsMap']['returnOnEquity'])
-        sys.stdout.flush()
+response = r.status_code
 
-if __name__ == '__main__':
-    main()
+if response == 200:
+	response = r.json()
+
+	json_data = response['resultMap']['PORTFOLIOS'][0]['portfolios'][0]
+
+
+	analyticsMap = json_data['analyticsMap']
+
+	listOfAnalytics = list()
+	for title in analyticsMap.keys():
+		listOfAnalytics.append(title)
+
+	analyticValues = list()
+	for analytic in listOfAnalytics:
+		analytic = analyticsMap[analytic]
+		name = analytic['name']
+		value = analytic['value']
+		harmonicMean = analytic['harmonicMean']
+
+		analyticValues.append([name, value, harmonicMean])
+
+
+	wantedAnalytic = input("What Analytic?: ")
+
+	for analyticList in analyticValues:
+		analytic = process.extractOne(analyticList[0], wantedAnalytic)
+		print(analytic)
+
+		print("Found Analytic: " + analyticList[0])
+		print("Found Value: " + str(value))
+		print("Found Harmonic Mean: " + str(harmonicMean))
+		break
+
+	expectedReturns = json_data['expectedReturns']
+	exposures = json_data['exposures']
+	holdings = json_data['holdings']
+	returns = json_data['returns']
+	riskData = json_data['riskData']
+	weightAsFraction = json_data['weightAsFraction']
