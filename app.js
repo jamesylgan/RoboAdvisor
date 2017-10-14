10bf9842674e589efbf01290c81a526a1fcafc5e
Aaron Kirtland <aaronk3.14@gmail.com>
2017-10-14 11:08:43 -0500

    Repeating dialogs done

--- app.js
+++ app.js
@@ -38,8 +38,14 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 // Main dialog with LUIS
-const LUIS_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/99e54023-bf56-4417-8311-fa5eb2d7245e?subscription-key=a659eee201024a6d92b6cf9a2973374a&timezoneOffset=0&verbose=true&spellCheck=true&q";
-var recognizer = new builder.LuisRecognizer(LUIS_URL);
+const LUIS_URL = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/99e54023-bf56-4417-8311-fa5eb2d7245e?subscription-key=a659eee201024a6d92b6cf9a2973374a&timezoneOffset=0&verbose=true&spellCheck=true&q=";
+// https://github.com/Microsoft/BotBuilder/issues/2670
+// enables not rezognizing for prompts
+// something says that IntentDialo may be better; switch to it if further problems?
+var recognizer = new builder.LuisRecognizer(LUIS_URL).onEnabled(function (context, callback) {
+     var enabled = context.dialogStack().length == 0;
+     callback(null, enabled);
+});
 bot.recognizer(recognizer);
 
 bot.dialog('Help', function (session) {
@@ -54,6 +60,44 @@ bot.dialog('Greeting', function (session) {
 }).triggerAction({
   matches: 'Greeting'
 });
+
+bot.dialog('Profile', [
+    function (session, args) {
+        // Save previous state (create on first call)
+        session.dialogData.index = (args === undefined) ? args.index : 0;
+        session.dialogData.form = (args === undefined) ? args.form : {};
+        console.log(session.dialogData.index);
+        console.log(session.dialogData.form);
+
+        // Prompt user for next field
+        builder.Prompts.text(session, questions[session.dialogData.index].prompt);
+    },
+    function (session, results) {
+        // Save users reply
+        var field = questions[session.dialogData.index++].field;
+        session.dialogData.form[field] = results.response;
+
+        // Check for end of form
+        if (session.dialogData.index >= questions.length) {
+          console.log("\n\nEND\n\n");
+            // Return completed form
+            session.userData.answers = session.dialogData.form;
+            // session.endDialog();
+            session.endDialogWithResult({ response: session.dialogData.form });
+        } else {
+            // Next field
+            session.replaceDialog('Profile', session.dialogData);
+        }
+    }
+]).triggerAction({
+  matches: 'Profile'
+});
+
+var questions = [
+    { field: 'name', prompt: "What's your name?" },
+    { field: 'age', prompt: "How old are you?" },
+    { field: 'state', prompt: "What state are you in?" }
+];
 
 // JS -> PYTHON code
 // https://gist.github.com/cowboy/3427148
@@ -78,3 +122,8 @@ var parent = function(session) {
     }
   });
 };
+
+// log any bot errors into the console
+bot.on('error', function (e) {
+    console.log('And error ocurred', e);
+});
