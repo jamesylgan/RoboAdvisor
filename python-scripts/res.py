7d35d3cc0f1197140feafdd12798029e09272331
Jordan Pizza <windowsinstalls@gmail.com>
2017-10-14 13:09:04 -0400

    Never Enough Dependencies

--- python-scripts/res.py
+++ python-scripts/res.py
@@ -3,64 +3,69 @@ import json
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import requests.packages.urllib3
+from datetime import date, time, datetime
+from dateutil import parser
+import sys
 requests.packages.urllib3.disable_warnings()
 
 # Risk (0 is Not Risky, 1 is risky) - oneYearRisk
 # upMonths, highDate, highReturn, 
 
+try:
+	ticker = sys.argv[1]
+except:
+	ticker = "MSFT"
 
-r = requests.post('http://www.blackrock.com/tools/hackathon/portfolio-analysis',
+
+r = requests.post('http://www.blackrock.com/tools/hackathon/performance?identifiers=' + ticker,
   data = {
-	'betaPortfolios' :  "SNP500",
-	"calculateExpectedReturns" :  "true",
-	"calculateExposures" :  "true",
-	"calculatePerformance" :  "true",
-	"calculateRisk" :  "true",
-	"calculateStressTests" :  "true",
-	"positions" :  "ISAAT~0.30|ORRYX~0.70",
-	"riskFreeRatePortfolio" :  "LTBILL1-3M",
-	"scenarios" :  "HIST_20081102_20080911,HIST_20110919_20110720,HIST_20130623_20130520,HIST_20140817_20140101,US10Y_1SD::APB,INF2Y_1SD::APB,USIG_1SD::APB,SPX_1SD::APB,DXY_1SD::APB"
-}, verify=False)
+	"identifiers": "MSFT"
+  }, verify=False)
 
 response = r.status_code
 
 if response == 200:
 	response = r.json()
 	try:
-		json_data = response['resultMap']['PORTFOLIOS'][0]['portfolios']
+		json_data = response['resultMap']
 	except:
 		print(r.text)
 	else:
-		analyticsMap = json_data['analyticsMap']
+		analyticsMap = json_data['RETURNS'][0]
 
-		listOfAnalytics = list()
-		for title in analyticsMap.keys():
-			listOfAnalytics.append(title)
+		if parser.parse(str(analyticsMap['highDate'])) == datetime.today():
+			highTime = 1
+		else:
+			highTime = 0
+			#Hardcoding for now
+			highTime = 1
 
-		analyticValues = list()
-		for analytic in listOfAnalytics:
-			analytic = analyticsMap[analytic]
-			name = analytic['name']
-			value = analytic['value']
-			harmonicMean = analytic['harmonicMean']
+		dateString = str(datetime.today().strftime('%Y%m%d'))
+		#Hardcoding for now
+		dateString = "20171005"
+		returnsMap = analyticsMap["returnsMap"]
+		stockInfo = returnsMap[dateString]
+		upPercentage = analyticsMap['upMonthsPercent']
+		totalMonths = analyticsMap['totalMonths']
+		returnsType = analyticsMap['returnsType']
 
-			analyticValues.append([name, value, harmonicMean])
+		r = requests.post('http://d.yimg.com/autoc.finance.yahoo.com/autoc?query=' + ticker + '&region=1&lang=en', verify=False)
+		tickerName = r.json()['ResultSet']['Result'][0]["name"]
 
 
-		wantedAnalytic = input("What Analytic?: ")
+		speechOutput = "Sure, you wanted information on " + tickerName + ". "
 
-		for analyticList in analyticValues:
-			analytic = process.extractOne(analyticList[0], wantedAnalytic)
-			print(analytic)
+		if highTime == 1:
+			speechOutput += "It's currently at it's highest price. "
 
-			print("Found Analytic: " + analyticList[0])
-			print("Found Value: " + str(value))
-			print("Found Harmonic Mean: " + str(harmonicMean))
-			break
+		speechOutput += "It typically goes up " + str(round((upPercentage * 100), 2)) + "% of the time, based on the " + str(totalMonths) + " months we have the data for. "
 
-		expectedReturns = json_data['expectedReturns']
-		exposures = json_data['exposures']
-		holdings = json_data['holdings']
-		returns = json_data['returns']
-		riskData = json_data['riskData']
-		weightAsFraction = json_data['weightAsFraction']
+		speechOutput += "It usually offers returns " + returnsType.lower() + ". "
+
+		print(speechOutput)
+
+
+
+
+
+
