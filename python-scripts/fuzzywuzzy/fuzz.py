05804a3a72d57d157808e1bb4fe4535885f88708
Jordan Pizza <windowsinstalls@gmail.com>
2017-10-15 01:39:36 -0400

    Fixed Fuzzywuzzy Warning

--- python-scripts/fuzzywuzzy/fuzz.py
+++ python-scripts/fuzzywuzzy/fuzz.py
@@ -31,8 +31,8 @@ import warnings
 try:
     from .StringMatcher import StringMatcher as SequenceMatcher
 except ImportError:
-    if platform.python_implementation() != "PyPy":
-        warnings.warn('Using slow pure-python SequenceMatcher. Install python-Levenshtein to remove this warning')
+    #if platform.python_implementation() != "PyPy":
+        #warnings.warn('Using slow pure-python SequenceMatcher. Install python-Levenshtein to remove this warning')
     from difflib import SequenceMatcher
 
 from . import utils
